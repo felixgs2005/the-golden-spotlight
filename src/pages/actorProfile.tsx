4438f5d0d3f4b4bb373760005f80c919514e7b88
Jessie Leclerc <jessie.leclerc21@gmail.com>
2025-11-05 15:27:43 -0500

    About Us page started

--- src/pages/actorProfile.tsx
+++
@@ -1,6 +0,0 @@
-import { useParams } from "react-router-dom";
-
-export default function ActorProfile() {
-  const { id } = useParams();
-  return <h1>Profil de l’acteur #{id}</h1>;
-}
--- src/types/tmdb/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./MovieTMDB";
-export * from "./CreditsTMDB";
